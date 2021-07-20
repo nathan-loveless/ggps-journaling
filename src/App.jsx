372d5551dbf2340b330289d5919c1ebacf211444
Nathan Loveless <nathansl2003@yahoo.com>
2021-07-20 13:47:17 -0400

    Initial commit with basic package.json loaded

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//import PrivateRoute from './utils/privateRoute';
+
+function App() {
+  return (
+    <>
+      Your rendering
+      <BrowserRouter>
+        <Switch></Switch>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App';
+//import reducer from './state/reducers/reducers';
+//import thunk from 'redux-thunk';
+//import { saveToLocalStorage, loadFromLocalStorage } from './utils/saveSession';
+
+require('dotenv').config();
+
+//const persistedState = loadFromLocalStorage();
+
+//const store = createStore(reducer, persistedState, applyMiddleware(thunk));
+
+//store.subscribe(() => {
+// saveToLocalStorage(store.getState());
+//});
+
+ReactDOM.render(
+  //<Provider store={store}>
+  <App />,
+  //</Provider>,
+  document.getElementById('root')
+);
