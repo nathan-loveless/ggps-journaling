8ace4d19af53ad7929f27a1df472bcedaa15fbad
Nathan Loveless <nathansl2003@yahoo.com>
2021-07-20 14:08:43 -0400

    Added redux state and tested

---
+++ src/state/reducers/reducers.js
@@ -0,0 +1,23 @@
+import { SIGNUP_SUBMIT } from '../actions/actions';
+const initialState = {
+  user: {
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: ''
+  }
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case SIGNUP_SUBMIT: {
+      console.log('Hit reducer signupSubmit');
+      return { ...state, user: action.payload.data };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export default reducer;
