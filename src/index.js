32d73d9665bf1907d11badaca3daba9b4d01ed8b
Nathan Loveless <nathansl2003@yahoo.com>
2021-08-02 19:20:30 -0400

    Moved amplify.js into source directory

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reducer from './state/reducers/reducers';
 import thunk from 'redux-thunk';
 import { saveToLocalStorage, loadFromLocalStorage } from './utils/saveSession';
-import { configureAmplify } from './amplify';
+import { configureAmplify } from './amplify.js';
 
 require('dotenv').config();
 
